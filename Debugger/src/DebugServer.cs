7e2e85f08ac181ed73f08a548a1a771e27c3e5aa
larikk <larikk.dev@gmail.com>
2025-05-31 21:59:41 +0200

    improve attache delay

--- Debugger/src/DebugServer.cs
+++ Debugger/src/DebugServer.cs
@@ -7,17 +7,16 @@ class DebugServer
     {
         int port = 41912;
         MoonSharpVsCodeDebugServer server = new(port);
-        server.Start();
 
         string scriptPath = Directory.GetCurrentDirectory() + "/TTSjson.lua";
         string scriptCode = File.ReadAllText(scriptPath);
-        Console.WriteLine(scriptCode);
         Script script = new();
 
         DynValue executionResult = script.DoString(scriptCode, null, scriptPath);
         server.AttachToScript(script, "TTSjson");
-
         var parseFunction = executionResult.Table.Get("parse").Function;
+
+        server.Start();
 
         Thread.Sleep(2000); // Give Debug Client enough time to attach before starting the parsing
         DynValue res = parseFunction.Call(str);
