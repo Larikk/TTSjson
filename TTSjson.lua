557ee5904cf797a1a3fcff4d25b67e378b420af6
larikk <larikk.dev@gmail.com>
2025-07-09 01:21:49 +0200

    switch to working on byte-level

--- TTSjson.lua
+++ TTSjson.lua
@@ -3,6 +3,36 @@ local module = {}
 local parseObject
 local parseArray
 
+local ASCII_SPACE = 0x20
+local ASCII_HORIZONTAL_TAB = 0x09  -- \t
+local ASCII_LINE_FEED = 0x0A       --\n
+local ASCII_CARRIAGE_RETURN = 0x0D -- \r
+local ASCII_DOUBLE_QUOTE = 0x22
+local ASCII_PLUS = 0x2B
+local ASCII_COMMA = 0x2C
+local ASCII_MINUS = 0x2D
+local ASCII_DOT = 0x2E
+local ASCII_FORWARDSLASH = 0x2F
+local ASCII_0 = 0x30
+local ASCII_9 = 0x39
+local ASCII_COLON = 0x3A
+local ASCII_UPPER_A = 0x41
+local ASCII_UPPER_E = 0x45
+local ASCII_UPPER_F = 0x46
+local ASCII_OPENING_SQARE_BRACKET = 0x5B  -- [
+local ASCII_BACKSLASH = 0x5C
+local ASCII_CLOSING_SQUARE_BRACKET = 0x5D --]
+local ASCII_LOWER_A = 0x61
+local ASCII_LOWER_B = 0x62
+local ASCII_LOWER_E = 0x65
+local ASCII_LOWER_F = 0x66
+local ASCII_LOWER_N = 0x6E
+local ASCII_LOWER_R = 0x72
+local ASCII_LOWER_T = 0x74
+local ASCII_LOWER_U = 0x75
+local ASCII_OPENING_CURLY_BRACE = 0x7B -- {
+local ASCII_CLOSING_CURLY_BRACE = 0x7D -- }
+
 local validHexDigits = {
     ["0"] = true,
     ["1"] = true,
@@ -31,36 +61,40 @@ local validHexDigits = {
 local lshift = bit32.lshift
 local extractBits = bit32.extract
 
-local function isDigit(c)
-    return c == "0" or c == "1" or c == "2" or c == "3" or c == "4" or c == "5" or c == "6" or c == "7" or c == "8" or
-        c == "9"
+local function isDigit(b)
+    return b ~= nil and b >= ASCII_0 and b <= ASCII_9
+end
+
+local function isHexDigit(b)
+    return b ~= nil and
+    (isDigit(b) or (b >= ASCII_UPPER_A and b <= ASCII_UPPER_F) or (b >= ASCII_LOWER_A and b <= ASCII_LOWER_F))
 end
 
 local function readChars(ctx, n)
     local tbl = {}
     for i = 1, n do
-        tbl[i] = ctx.currentChar
-        ctx.advanceChar()
+        tbl[i] = ctx.currentChar()
+        ctx.nextByte()
     end
     return table.concat(tbl)
 end
 
 local function parseBoolean(ctx)
-    if ctx.currentChar == "t" then
+    if ctx.currentByte == ASCII_LOWER_T then
         local s = readChars(ctx, 4)
         if s ~= "true" then error("expected true, got " .. s) end
         return true
-    elseif ctx.currentChar == "f" then
+    elseif ctx.currentByte == ASCII_LOWER_F then
         local s = readChars(ctx, 5)
         if s ~= "false" then error("expected false, got " .. s) end
         return false
     else
-        error("expected start of boolean, got " .. ctx.currentChar)
+        error("expected start of boolean, got " .. ctx.currentChar())
     end
 end
 
 local function parseNull(ctx)
-    if ctx.currentChar ~= "n" then error("expected start of null, got " .. ctx.currentChar) end
+    if ctx.currentByte ~= ASCII_LOWER_N then error("expected start of null, got " .. ctx.currentChar()) end
     local s = readChars(ctx, 4)
 
     if s == "null" then
@@ -71,16 +105,17 @@ local function parseNull(ctx)
 end
 
 local function parseNumber(ctx)
-    if ctx.currentChar ~= "-" and not isDigit(ctx.currentChar) then
-        error("expected start of number, got " .. ctx.currentChar)
+    if ctx.currentByte ~= ASCII_MINUS and not isDigit(ctx.currentByte) then
+        error("expected start of number, got " .. ctx.currentChar())
     end
 
+    -- todo check for further optimization
     local tbl = {}
     local tblPos = 1
-    while isDigit(ctx.currentChar) or ctx.currentChar == "-" or ctx.currentChar == "+" or ctx.currentChar == "." or ctx.currentChar == "e" or ctx.currentChar == "E" do
-        tbl[tblPos] = ctx.currentChar
+    while isDigit(ctx.currentByte) or ctx.currentByte == ASCII_MINUS or ctx.currentByte == ASCII_PLUS or ctx.currentByte == ASCII_DOT or ctx.currentByte == ASCII_LOWER_E or ctx.currentByte == ASCII_UPPER_E do
+        tbl[tblPos] = string.char(ctx.currentByte)
         tblPos = tblPos + 1
-        ctx.advanceChar()
+        ctx.nextByte()
     end
 
     local s = table.concat(tbl)
@@ -91,18 +126,19 @@ local function parseNumber(ctx)
 end
 
 local function parseUnicodeSeq(ctx)
-    if ctx.currentChar ~= "u" then error("expected start of unicode sequence, got " .. ctx.currentChar) end
-    ctx.advanceChar()
+    if ctx.currentByte ~= ASCII_LOWER_U then error("expected start of unicode sequence, got " .. ctx.currentChar()) end
+    ctx.nextByte()
 
+    -- todo check for more optimization
     local hexDigits = {}
     local isValidHex = true
     for i = 1, 4 do
-        local c = ctx.currentChar
-        hexDigits[i] = c
-        if not validHexDigits[c] then
+        local b = ctx.currentByte
+        hexDigits[i] = ctx.currentChar()
+        if not isHexDigit(b) then
             isValidHex = false
         end
-        ctx.advanceChar()
+        ctx.nextByte()
     end
 
     local hex = table.concat(hexDigits)
@@ -115,15 +151,14 @@ local function parseUnicodeSeq(ctx)
 end
 
 local function parseUtf8(ctx)
-    local c = ctx.currentChar
-    local b1 = string.byte(c, 1)
-    ctx.advanceChar()
+    local b1 = ctx.currentByte
+    ctx.nextByte()
 
     local numberOfBytes
     if b1 <= 0x1F or b1 == 0x7F then
         error("unescaped control character encountered: 0x" .. string.format("%02X", b1))
     elseif b1 <= 0x7F then
-        return c
+        return string.char(b1)
     elseif b1 <= 0xDF then
         numberOfBytes = 2
     elseif b1 <= 0xEF then
@@ -131,13 +166,13 @@ local function parseUtf8(ctx)
     elseif b1 <= 0xF7 then
         numberOfBytes = 4
     else
-        error("expected valid byte for utf-8 codepoint, got" .. ctx.currentChar)
+        error("expected valid byte for utf-8 codepoint, got 0x" .. string.format("%02X", b1))
     end
 
     local tbl = { b1 }
     for i = 2, numberOfBytes do
-        tbl[i] = string.byte(ctx.currentChar, 1)
-        ctx.advanceChar()
+        tbl[i] = ctx.currentByte
+        ctx.nextByte()
     end
 
     -- Calculate U+wxyz
@@ -166,8 +201,8 @@ local function parseUtf8(ctx)
 end
 
 local function parseString(ctx)
-    if ctx.currentChar ~= "\"" then error("expected start of string, got " .. ctx.currentChar) end
-    ctx.advanceChar()
+    if ctx.currentByte ~= ASCII_DOUBLE_QUOTE then error("expected start of string, got " .. ctx.currentChar()) end
+    ctx.nextByte()
     local done = false
     local escaped = false
     local sb = {}
@@ -178,38 +213,44 @@ local function parseString(ctx)
     end
 
     while not done do
-        local c = ctx.currentChar
+        local b = ctx.currentByte
         if escaped then
-            if c == "\"" or c == "\\" or c == "/" then
-                push(c)
-                ctx.advanceChar()
-            elseif c == "n" then
+            if b == ASCII_DOUBLE_QUOTE then
+                push("\"")
+                ctx.nextByte()
+            elseif b == ASCII_BACKSLASH then
+                push("\\")
+                ctx.nextByte()
+            elseif b == ASCII_FORWARDSLASH then
+                push("/")
+                ctx.nextByte()
+            elseif b == ASCII_LOWER_N then
                 push("\n")
-                ctx.advanceChar()
-            elseif c == "u" then
+                ctx.nextByte()
+            elseif b == ASCII_LOWER_U then
                 push(parseUnicodeSeq(ctx))
-            elseif c == "r" then
+            elseif b == ASCII_LOWER_R then
                 push("\r")
-                ctx.advanceChar()
-            elseif c == "t" then
+                ctx.nextByte()
+            elseif b == ASCII_LOWER_T then
                 push("\t")
-                ctx.advanceChar()
-            elseif c == "b" then
+                ctx.nextByte()
+            elseif b == ASCII_LOWER_B then
                 push("\b")
-                ctx.advanceChar()
-            elseif c == "f" then
+                ctx.nextByte()
+            elseif b == ASCII_LOWER_F then
                 push("\f")
-                ctx.advanceChar()
+                ctx.nextByte()
             else
-                error("unsupported escaped symbol " .. c)
+                error("unsupported escaped symbol " .. ctx.currentChar())
             end
             escaped = false
-        elseif c == "\\" then
+        elseif b == ASCII_BACKSLASH then
             escaped = true
-            ctx.advanceChar()
-        elseif c == "\"" then
+            ctx.nextByte()
+        elseif b == ASCII_DOUBLE_QUOTE then
             done = true
-            ctx.advanceChar()
+            ctx.nextByte()
         else
             push(parseUtf8(ctx))
         end
@@ -219,23 +260,23 @@ local function parseString(ctx)
 end
 
 local function parseValue(ctx)
-    local c = ctx.currentChar
+    local b = ctx.currentByte
     local value
 
-    if c == "\"" then
+    if b == ASCII_DOUBLE_QUOTE then
         value = parseString(ctx)
-    elseif c == "{" then
+    elseif b == ASCII_OPENING_CURLY_BRACE then
         value = parseObject(ctx)
-    elseif c == "[" then
+    elseif b == ASCII_OPENING_SQARE_BRACKET then
         value = parseArray(ctx)
-    elseif c == "f" or c == "t" then
+    elseif b == ASCII_LOWER_F or b == ASCII_LOWER_T then
         value = parseBoolean(ctx)
-    elseif c == "-" or isDigit(c) then
+    elseif b == ASCII_MINUS or isDigit(b) then
         value = parseNumber(ctx)
-    elseif c == "n" then
+    elseif b == ASCII_LOWER_N then
         value = parseNull(ctx)
     else
-        error("expected start of a value, got " .. c)
+        error("expected start of a value, got " .. ctx.currentChar())
     end
 
     ctx.skipWhiteSpace()
@@ -244,12 +285,12 @@ local function parseValue(ctx)
 end
 
 parseObject = function(ctx)
-    if ctx.currentChar ~= "{" then error("expected start of object, got " .. ctx.currentChar) end
-    ctx.advanceChar()
+    if ctx.currentByte ~= ASCII_OPENING_CURLY_BRACE then error("expected start of object, got " .. ctx.currentChar()) end
+    ctx.nextByte()
     ctx.skipWhiteSpace()
 
-    if ctx.currentChar == "}" then
-        ctx.advanceChar()
+    if ctx.currentByte == ASCII_CLOSING_CURLY_BRACE then
+        ctx.nextByte()
         ctx.skipWhiteSpace()
         return {}
     end
@@ -259,20 +300,20 @@ parseObject = function(ctx)
     while true do
         local key = parseString(ctx)
         ctx.skipWhiteSpace()
-        if ctx.currentChar ~= ":" then error("expected :, got " .. ctx.currentChar) end
-        ctx.advanceChar()
+        if ctx.currentByte ~= ASCII_COLON then error("expected :, got " .. ctx.currentChar()) end
+        ctx.nextByte()
         ctx.skipWhiteSpace()
         local value = parseValue(ctx)
         obj[key] = value
-        if ctx.currentChar == "," then
-            ctx.advanceChar()
+        if ctx.currentByte == ASCII_COMMA then
+            ctx.nextByte()
             ctx.skipWhiteSpace()
-        elseif ctx.currentChar == "}" then
-            ctx.advanceChar()
+        elseif ctx.currentByte == ASCII_CLOSING_CURLY_BRACE then
+            ctx.nextByte()
             ctx.skipWhiteSpace()
             return obj
         else
-            error("expected ',' or '}' after object value but got " .. ctx.currentChar)
+            error("expected ',' or '}' after object value but got " .. ctx.currentChar())
         end
     end
 
@@ -280,12 +321,12 @@ parseObject = function(ctx)
 end
 
 parseArray = function(ctx)
-    if ctx.currentChar ~= "[" then error("expected start of array, got " .. ctx.currentChar) end
-    ctx.advanceChar()
+    if ctx.currentByte ~= ASCII_OPENING_SQARE_BRACKET then error("expected start of array, got " .. ctx.currentChar()) end
+    ctx.nextByte()
     ctx.skipWhiteSpace()
 
-    if ctx.currentChar == "]" then
-        ctx.advanceChar()
+    if ctx.currentByte == ASCII_CLOSING_SQUARE_BRACKET then
+        ctx.nextByte()
         ctx.skipWhiteSpace()
         return {}
     end
@@ -297,15 +338,15 @@ parseArray = function(ctx)
         local value = parseValue(ctx)
         tbl[tblPos] = value
         tblPos = tblPos + 1
-        if ctx.currentChar == "," then
-            ctx.advanceChar()
+        if ctx.currentByte == ASCII_COMMA then
+            ctx.nextByte()
             ctx.skipWhiteSpace()
-        elseif ctx.currentChar == "]" then
-            ctx.advanceChar()
+        elseif ctx.currentByte == ASCII_CLOSING_SQUARE_BRACKET then
+            ctx.nextByte()
             ctx.skipWhiteSpace()
             return tbl
         else
-            error("expected ',' or ']' after array value but got " .. ctx.currentChar)
+            error("expected ',' or ']' after array value but got " .. ctx.currentChar())
         end
     end
 
@@ -317,17 +358,27 @@ function module.parse(str)
     ctx.pos = 1
     ctx.buffer = str
     ctx.bufferLen = #str
-    ctx.currentChar = string.sub(str, 1, 1)
-    ctx.advanceChar = function()
-        if ctx.currentChar == "" then error("json is not terminated properly") end
+    ctx.currentByte = string.byte(str, 1)
+    ctx.nextByte = function()
+        if ctx.currentByte == nil then error("json is not terminated properly") end
         ctx.pos = ctx.pos + 1
-        ctx.currentChar = string.sub(ctx.buffer, ctx.pos, ctx.pos)
+        ctx.currentByte = string.byte(ctx.buffer, ctx.pos)
+        return ctx.currentByte
+    end
+    ctx.currentChar = function()
+        local b = ctx.currentByte
+        if (b == nil) then return "" end
+        return string.char(b)
     end
     ctx.skipWhiteSpace = function()
-        local c = ctx.currentChar
-        while c == " " or c == "\t" or c == "\n" or c == "\r" do
-            ctx.advanceChar()
-            c = ctx.currentChar
+        local b = ctx.currentByte
+        while
+            b == ASCII_SPACE
+            or b == ASCII_HORIZONTAL_TAB
+            or b == ASCII_LINE_FEED
+            or b == ASCII_CARRIAGE_RETURN
+        do
+            b = ctx.nextByte()
         end
     end
 
@@ -335,7 +386,7 @@ function module.parse(str)
     local value = parseValue(ctx)
     ctx.skipWhiteSpace()
 
-    if ctx.currentChar ~= "" then error("json has data past the parsed value") end
+    if ctx.currentByte ~= nil then error("json has data past the parsed value") end
 
     return value
 end
