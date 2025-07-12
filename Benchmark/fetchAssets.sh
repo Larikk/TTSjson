c5808fd5d2d1b3972f70e300378e5ee5f1984319
larikk <larikk.dev@gmail.com>
2025-07-12 14:45:42 +0200

    bigger benchmark data set

--- Benchmark/fetchAssets.sh
+++ Benchmark/fetchAssets.sh
@@ -1,3 +1,3 @@
-curl --output benchmark.json "https://db.ygoprodeck.com/api/v7/cardinfo.php?attribute=wind&enddate=2015-01-01"
+curl --output benchmark.json "https://db.ygoprodeck.com/api/v7/cardinfo.php?attribute=wind&enddate=2018-01-01"
 curl --output TTSNativeJSON.ttslua "https://raw.githubusercontent.com/Berserk-Games/Tabletop-Simulator-Lua-Classes/refs/heads/master/JSON/JSON.lua"
 sed -i 's/""/"/g' TTSNativeJSON.ttslua # The file on github has doubled quotes for some reason
