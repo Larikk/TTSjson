c6399140fcbcb338f6f934847f6b765b49e30c42
larikk <larikk.dev@gmail.com>
2025-07-20 15:42:25 +0200

    more tests

--- Tests/tests/Writing/ObjectTableWriteTests.cs
+++ Tests/tests/Writing/ObjectTableWriteTests.cs
@@ -1,4 +1,6 @@
 using FluentAssertions;
+using FluentAssertions.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Tests.Tests.Writing;
 
@@ -11,7 +13,40 @@ public class ObjectTableWriteTests
     {
         var actual = ttsjson.EvalWrite(@"{key = ""value""}");
         var expected = @"{""key"":""value""}";
-        actual.Should().BeEquivalentTo(expected);
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void ShouldWriteNestedObjectTable()
+    {
+        var actual = ttsjson.EvalWrite(@"{key = {key = ""value""}}");
+        var expected = @"{""key"":{""key"":""value""}}";
+        actual.Should().Be(expected);
+    }
+
+    [Fact]
+    public void ShouldWriteTableWithAllPossibleValueTypesExceptNil()
+    {
+        var actual = ttsjson.EvalWrite("""
+            {
+                _nil = nil,
+                _true = true,
+                _false = false,
+                _number = 123,
+                _array = {1, 2, 3},
+                _object = {key = "value"},
+            }
+        """);
+        var expected = """
+            {
+                "_true": true,
+                "_false": false,
+                "_number": 123,
+                "_array": [1, 2, 3],
+                "_object": {"key": "value"}
+            }
+        """;
+        JToken.Parse(actual).Should().BeEquivalentTo(JToken.Parse(expected));
     }
 
 }
