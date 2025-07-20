99a30b9ca0f0f77ad6a61cf77af1b30d47bf9372
larikk <larikk.dev@gmail.com>
2025-07-20 13:36:55 +0200

    enable debugging of json writing

--- Debugger/src/Main.cs
+++ Debugger/src/Main.cs
@@ -1,2 +1,9 @@
+using MoonSharp.Interpreter;
+
 var debugServer = new TTSjsonDebugServer();
-debugServer.DebugParsing("true");
+// debugServer.DebugParsing("true");
+
+var table = new Table(null);
+table["key"] = "string";
+debugServer.DebugWriting(DynValue.NewTable(table));
+
