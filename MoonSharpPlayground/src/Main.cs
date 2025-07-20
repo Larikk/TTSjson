9f08d42f7594b15976de3f5c2505ff66373fb267
larikk <larikk.dev@gmail.com>
2025-07-20 14:02:41 +0200

    remove async task in playground

--- MoonSharpPlayground/src/Main.cs
+++ MoonSharpPlayground/src/Main.cs
@@ -1,19 +1,11 @@
 ﻿using MoonSharp.Interpreter;
 
+string scriptCode = """
+print("Hello")
+return "World"
+""";
 
-static void test()
-{
-    string scriptCode = """
-    print("Hello")
-    return "World"
-    """;
-
-    var script = new Script();
-    script.Options.DebugPrint = Console.WriteLine;
-    var res = script.DoString(scriptCode);
-    Console.WriteLine(res);
-}
-
-Task task = Task.Run(() => test());
-var ct = new CancellationTokenSource(TimeSpan.FromMilliseconds(200)).Token;
-await task.WaitAsync(ct);
+var script = new Script();
+script.Options.DebugPrint = Console.WriteLine;
+var res = script.DoString(scriptCode);
+Console.WriteLine(res);
